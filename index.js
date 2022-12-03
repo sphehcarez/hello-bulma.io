a893e54716cc502dee85c8b1e2734cfda446256e
sphesihle <sphehcarez@gmail.com>
2022-12-04 00:16:14 +0200

    updates

---
+++ index.js
@@ -0,0 +1,7 @@
+// mobile view
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () =>{
+    navbarMenu.classList.toggle('is-active')
+})
